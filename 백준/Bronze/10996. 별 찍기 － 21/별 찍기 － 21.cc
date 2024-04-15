4c7cbcf267abdf963bd96f69c4b74e18eb21aab6
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-04-15 11:29:31 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	for(int i=0;i<n*2;i++){
+		for(int j=0;j<n;j++){
+			if(i%2==0){
+				if(j%2==0){
+					cout<<"*";
+				}
+				else{
+					cout<<" ";
+				}
+			}
+			else{
+				if(j%2!=0){
+					cout<<"*";
+				}
+				else{
+					cout<<" ";
+				}
+			}
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
