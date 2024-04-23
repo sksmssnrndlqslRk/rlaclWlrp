e37418169ed087b0d2dc0f5acc187b3a244804ec
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-04-23 15:06:47 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a+b+c+d+e;
+	
+	return 0;
+}
