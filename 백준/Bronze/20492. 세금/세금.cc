4e05df2950fc80bf18c9c46f4354d05bb327c01d
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-04-25 20:20:24 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	cout<<int(n*(0.78))<<"\n"<<int(n*0.8 + n*0.2*0.78);
+	return 0;
+}
