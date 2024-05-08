0382ec2a701476c780078836144f4fb0613dfcd2
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-05-09 08:16:45 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	char a;
+	cin>>a;
+	cout<<((a=='N'||a=='n') ? "Naver D2":"Naver Whale");
+	return 0;
+}
