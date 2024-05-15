5a701f858d783994827c07c9060a8c376ce552ed
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-05-16 08:40:06 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int ans;
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    ans=a*6+b*3+c*2+d*1+e*2;
+    cin>>a>>b>>c>>d>>e;
+    cout<<ans<<" ";
+    cout<<a*6+b*3+c*2+d*1+e*2;
+    return 0;
+}
