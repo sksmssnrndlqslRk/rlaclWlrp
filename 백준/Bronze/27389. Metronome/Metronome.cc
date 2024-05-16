6c63698d54f7ea38b666de7b29dd340b2058688a
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-05-16 21:57:10 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a;
+    cin>>a;
+    cout<<fixed<<setprecision(2)<<a/4.0;
+    return 0;
+}
