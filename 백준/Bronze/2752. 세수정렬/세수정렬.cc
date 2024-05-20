bc366db0eb26243e2f595efe4301313bffdecb19
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-05-20 19:11:47 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> vec;
+    int a;
+    for(int i=0;i<3;i++){
+        cin>>a;
+        vec.push_back(a);
+    }
+    sort(vec.begin(),vec.end());
+    cout<<vec[0]<<" "<<vec[1]<<" "<<vec[2];
+    return 0;
+}
