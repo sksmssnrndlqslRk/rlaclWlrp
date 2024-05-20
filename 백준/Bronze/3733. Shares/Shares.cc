26c1ab52cb37b3a99f52a46af2bf529da49263b9
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-05-20 19:06:14 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b;
+    while(cin>>a>>b){
+        cout<<b/(a+1)<<"\n";
+    }
+    return 0;
+}
