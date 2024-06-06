0ec64b1029d2e5883095a488128b9c297c9cfd69
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-06-07 00:24:26 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<a*56+b*24+c*14+d*6;
+    return 0;
+}
