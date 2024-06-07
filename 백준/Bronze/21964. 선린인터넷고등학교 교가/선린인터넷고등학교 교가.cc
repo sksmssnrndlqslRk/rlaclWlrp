4bd30e1fcc3ae3cedc923498ffdf54f456b834d2
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-06-07 20:37:51 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string a;
+    int t;
+    cin>>t>>a;
+    for(int i=t-5;i<t;i++){
+        cout<<a[i];
+    }
+    return 0;
+}
