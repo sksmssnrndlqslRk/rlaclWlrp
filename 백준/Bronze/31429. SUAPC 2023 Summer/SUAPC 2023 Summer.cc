825157c1e762bb3381e47b1cc1d8ea0d972859e2
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-06-13 19:02:32 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int a[50]={12,11,11,10,9,9,9,8,7,6,6};
+	int ab[50]={1600,894,1327,1311,1004,1178,1357,837,1055,556,773};
+	int n;
+	cin>>n;
+	cout<<a[n-1]<<" "<<ab[n-1];
+	return 0;
+}
