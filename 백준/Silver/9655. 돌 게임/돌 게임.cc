548c95a678388ad76068e939d5d0b16729dbf9a6
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-06-19 10:16:03 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int n;
+    cin>>n;
+    
+	if(n% 2==1) {
+		cout<<"SK";
+	}
+	else{
+		cout<<"CY";
+	}
+
+	return 0;
+}
