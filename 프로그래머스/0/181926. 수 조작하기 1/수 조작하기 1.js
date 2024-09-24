34a850906d51257174ca1763b0cefd2e6ab4b9c2
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-09-24 14:56:14 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.64 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,19 @@
+function solution(n, control) {
+    var answer=n;
+     for(var i=0;i<control.length;i++){
+        if(control[i]==="w"){
+            answer+=1;
+        }
+        else if(control[i]==="s"){
+            answer-=1;
+        }
+        else if(control[i]==="d"){
+            answer+=10;
+        }
+        else if(control[i]==="a"){
+            answer-=10;
+        }
+    }
+    return answer;
+
+}
