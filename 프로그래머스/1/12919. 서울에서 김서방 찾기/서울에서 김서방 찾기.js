af11f7cfa92edece933ca3296722057949fd5117
sksmssnrndlqslRk <142009423+sksmssnrndlqslRk@users.noreply.github.com>
2024-11-19 15:02:04 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,14 @@
+function solution(seoul) {
+    var answer = '';
+    
+    for(var i=0;i<seoul.length;i++){
+        if(seoul[i]=="Kim"){
+            answer="김서방은 ";
+            answer+=String(i);
+            answer+="에 있다"
+               return answer;         
+        } 
+
+    }
+
+}
